0d9ba449813e5a94e2e581f006667e36ec8af2bd
Shambhavi Sachid <shambu1985@gmail.com>
2014-07-11 23:12:52 -0700

    add more code
    ruby_prac file

--- ruby_prac.rb
+++ ruby_prac.rb
@@ -19,23 +19,6 @@ p 2.euro
 p 2.rupees
 p 3.pounds
 
-# @@currencies is a hash which just maps currency names to conversion factors				
-# One of the facilities of Ruby is that method missing gets called when you call a method on 
-# the receiver and the receiver doesn't know any method by that name, but if you have defined a method
-# called method_missing in your class, it will receive the call instead. Method_missing is passed 
-# upto 3 arguments. The first one is the actual name
-# of the method you tried to call, but which doesn't appear to
-# exist, a list of the arguments that that method would have
-# received if it existed, and optionally a block. Most Ruby methods are
-# written so that they can take a block as an argument, 
-# What method missing does in our case is, it looks at the method
-# ID, converts it to a string, gets rid of any final s ...
-# remember we had the problem of wanting to be able to call 1.
-# Euro as well as 1. Euros ... so we're basically going to
-# canonicalize the name of a currency to its singular, and then we
-# just ask whether we already have a key for that currency and we
-# convert it. 
-
 
 
 
